307165afb7b8db6316b833b1cfaf817ce80fb5a9
imadfen <imadfen20@gmail.com>
2023-12-03 22:51:36 +0100

    styled the tabs heads

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,14 +3,18 @@ package app;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 
 class Todo {
@@ -44,7 +48,27 @@ class Todo {
 }
 
 public class Main extends Application {
-    private String iconUrl = "icon.png";
+    private int page = 0;
+
+    private void updateTabsStyle(Label tab1, Label tab2, Label tab3){
+        tab1.getStyleClass().remove("selected-tab");
+        tab2.getStyleClass().remove("selected-tab");
+        tab3.getStyleClass().remove("selected-tab");
+
+        switch (page) {
+            case 0:
+                tab1.getStyleClass().add("selected-tab");
+                break;
+            case 1:
+                tab2.getStyleClass().add("selected-tab");
+                break;
+            case 2:
+                tab3.getStyleClass().add("selected-tab");
+                break;
+            default:
+                break;
+        }
+    }
 
     @Override
     public void start(Stage stage) {
@@ -56,37 +80,73 @@ public class Main extends Application {
         };
 
 
-//        StackPane root = new StackPane();
-        VBox vBox = new VBox();
+        // titre:
+        Label title = new Label("Liste De Tâches");
+        title.getStyleClass().add("title");
+
+        // pages:
+        HBox tabs = new HBox();
+
+        Label tab1 = new Label("Tout");
+        Label tab2 = new Label("Active");
+        Label tab3 = new Label("Terminé");
+        updateTabsStyle(tab1, tab2, tab3);
+
+        tab1.setOnMouseClicked(event -> {page = 0; updateTabsStyle(tab1, tab2, tab3);});
+        tab2.setOnMouseClicked(event -> {page = 1; updateTabsStyle(tab1, tab2, tab3);});
+        tab3.setOnMouseClicked(event -> {page = 2; updateTabsStyle(tab1, tab2, tab3);});
+
+        tab1.getStyleClass().add("tab-label");
+        tab2.getStyleClass().add("tab-label");
+        tab3.getStyleClass().add("tab-label");
 
 
-        HBox hBox = new HBox();
+        Region spacer1 = new Region();
+        Region spacer2 = new Region();
+        Region spacer3 = new Region();
+        Region spacer4 = new Region();
+
+        // Set HGrow to ALWAYS for spacer nodes
+        HBox.setHgrow(spacer1, javafx.scene.layout.Priority.ALWAYS);
+        HBox.setHgrow(spacer2, javafx.scene.layout.Priority.ALWAYS);
+        HBox.setHgrow(spacer3, javafx.scene.layout.Priority.ALWAYS);
+        HBox.setHgrow(spacer4, javafx.scene.layout.Priority.ALWAYS);
+
+        tabs.getChildren().addAll(spacer1, tab1, spacer2, tab2, spacer3, tab3, spacer4);
+        tabs.setAlignment(Pos.CENTER);
+        tabs.getStyleClass().add("tabs");
+
+        VBox mainVBox = new VBox();
+        mainVBox.setAlignment(Pos.TOP_CENTER);
+
+        // liste des taches:
+        ListView<String> listView = new ListView<>();
+        listView.getStyleClass().add("list");
 
         for (Todo todo : todos) {
-            Label label = new Label(todo.getValue());
-            vBox.getChildren().add(label);
+            listView.getItems().add(todo.getValue());
         }
 
-
-        hBox.getChildren().add(vBox);
-
+        mainVBox.getChildren().addAll(title, tabs, listView);
 
         // Scene:
-        Scene scene = new Scene(hBox);
+        Scene scene = new Scene(mainVBox);
+
+        // importer css:
+        String cssFileUrl = "style.css";
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource(cssFileUrl)).toExternalForm());
 
 
         // Stage:
             // icon de stage:
+        String iconUrl = "icon.png";
         Image icon = new Image(iconUrl);
         stage.getIcons().add(icon);
 
             // taille de stage:
-        stage.setHeight(450);
-        stage.setMinHeight(400);
-        stage.setMaxHeight(500);
-        stage.setWidth(350);
-        stage.setMinWidth(300);
-        stage.setMaxWidth(450);
+        stage.setHeight(650);
+        stage.setWidth(550);
+        stage.setResizable(false);
 
         stage.setTitle("Liste De Tâches");
         stage.setScene(scene);
