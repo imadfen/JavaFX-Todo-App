cb0e40d7099323afa115129e3745a85b2fb30381
imadfen <imadfen20@gmail.com>
2023-12-06 22:01:41 +0100

    finished the add new todo functionnality

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,18 +1,19 @@
 package app;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
 import java.util.Objects;
 
 
@@ -81,14 +82,69 @@ public class Main extends Application {
         }
     }
 
-    @Override
-    public void start(Stage stage) {
-        Todo[] todos = new Todo[30];
+    private void createTodo(Todo todo, ListView<HBox> listView){
+        HBox todoBox = new HBox();
 
-        for (int i = 0; i < 30; i++) {
-            todos[i] = new Todo("todo number " + (i+1));
+        // checkbox:
+        CheckBox checkBox = new CheckBox();
+        // initialisation de l'etat de checkbox:
+        checkBox.setSelected(todo.getIsChecked());
+
+        // espace vide:
+        Region todoSpacer = new Region();
+        todoSpacer.setPrefWidth(10);
+
+        // text de tache:
+        Text todoText = new Text(todo.getValue());
+        todoText.getStyleClass().add("todo-text");
+        todoText.setWrappingWidth(460);
+        // initialisation de l'etat de text de tache:
+        if (todo.getIsChecked()){
+            todoText.getStyleClass().add("text-checked");
+        } else {
+            todoText.getStyleClass().remove("text-checked");
         }
 
+        // set la fonctionnalite de click de checkbox:
+        checkBox.setOnAction(event -> {
+            toggleTodo(todo, checkBox, todoText);
+        });
+
+        // set la fonctionnalite de click de text de tache:
+        todoText.setOnMouseClicked(event -> {
+            toggleTodo(todo, checkBox, todoText);
+        });
+
+
+        // ajoute les element de tache dans le conteneur:
+        todoBox.getChildren().addAll(checkBox, todoSpacer, todoText);
+
+        // ajoute le conteneur dans le ListView:
+        listView.getItems().addAll(todoBox);
+    }
+
+    private void addNewTodo(ArrayList<Todo> todos, ListView<HBox> listView, TextField todoInput){
+        String newTodoText = todoInput.getText();
+        if (!newTodoText.isEmpty()){
+            todos.add(new Todo(newTodoText));
+            createTodo(todos.get(todos.size()-1), listView);
+            todoInput.clear();
+            listView.scrollTo(todos.size() - 1);
+        }
+    }
+
+    @Override
+    public void start(Stage stage) {
+        ArrayList<Todo> todos = new ArrayList<>();
+
+        for (int i = 0; i < 30; i++) {
+            todos.add(new Todo("todo number " + (i+1)));
+        }
+
+        // conteneur principale des elements de l'app:
+        VBox mainVBox = new VBox();
+        mainVBox.setAlignment(Pos.TOP_CENTER);
+        mainVBox.setPadding(new Insets(10, 0, 10, 0));
 
         // titre:
         Label title = new Label("Liste De Tâches");
@@ -110,66 +166,62 @@ public class Main extends Application {
         tab2.getStyleClass().add("tab-label");
         tab3.getStyleClass().add("tab-label");
 
-
+        // des espaces:
         Region spacer1 = new Region();
         Region spacer2 = new Region();
         Region spacer3 = new Region();
         Region spacer4 = new Region();
 
-        // Set HGrow to ALWAYS for spacer nodes
-        HBox.setHgrow(spacer1, javafx.scene.layout.Priority.ALWAYS);
-        HBox.setHgrow(spacer2, javafx.scene.layout.Priority.ALWAYS);
-        HBox.setHgrow(spacer3, javafx.scene.layout.Priority.ALWAYS);
-        HBox.setHgrow(spacer4, javafx.scene.layout.Priority.ALWAYS);
+        HBox.setHgrow(spacer1, Priority.ALWAYS);
+        HBox.setHgrow(spacer2, Priority.ALWAYS);
+        HBox.setHgrow(spacer3, Priority.ALWAYS);
+        HBox.setHgrow(spacer4, Priority.ALWAYS);
 
         tabs.getChildren().addAll(spacer1, tab1, spacer2, tab2, spacer3, tab3, spacer4);
         tabs.setAlignment(Pos.CENTER);
         tabs.getStyleClass().add("tabs");
 
-        VBox mainVBox = new VBox();
-        mainVBox.setAlignment(Pos.TOP_CENTER);
-
         // liste des taches:
-        ListView<HBox> listView = new ListView<>();
-        listView.getStyleClass().add("list");
+            // initialisation de ListView:
+        ListView<HBox> todoListView = new ListView<>();
+        todoListView.getStyleClass().add("list");
+        VBox.setVgrow(todoListView, Priority.ALWAYS);
 
-
-        todos[0].toggleCheck();
-
+            // fournir les taches dans ListView:
         for (Todo todo : todos) {
-            HBox todoBox = new HBox();
-            CheckBox checkBox = new CheckBox();
-
-            Region todoSpacer = new Region();
-            todoSpacer.setPrefWidth(10);
-
-            Text todoText = new Text(todo.getValue());
-            todoText.getStyleClass().add("todo-text");
-
-            checkBox.setSelected(todo.getIsChecked());
-            if (todo.getIsChecked()){
-                todoText.getStyleClass().add("text-checked");
-            } else {
-                todoText.getStyleClass().remove("text-checked");
-            }
-
-            checkBox.setOnAction(event -> {
-                toggleTodo(todo, checkBox, todoText);
-            });
-
-            todoText.setOnMouseClicked(event -> {
-                toggleTodo(todo, checkBox, todoText);
-            });
-
-
-            todoBox.getChildren().addAll(checkBox, todoSpacer, todoText);
-
-            listView.getItems().addAll(todoBox);
+            createTodo(todo, todoListView);
         }
 
-        mainVBox.getChildren().addAll(title, tabs, listView);
 
 
+        HBox addTodoBox = new HBox();
+        addTodoBox.setPadding(new Insets(0, 10, 0, 10));
+        addTodoBox.setPrefHeight(50);
+        addTodoBox.setAlignment(Pos.CENTER);
+
+        TextField addTodoInput = new TextField();
+        addTodoInput.setPromptText("ajouter tâche...");
+        addTodoInput.getStyleClass().add("new-todo-input");
+        HBox.setHgrow(addTodoInput, Priority.ALWAYS);
+        addTodoInput.setOnAction(e -> {
+            addNewTodo(todos, todoListView, addTodoInput);
+        });
+
+        Region newTodoSpacer = new Region();
+        newTodoSpacer.setPrefWidth(10);
+
+        Button submitButton = new Button("Ajouter");
+        submitButton.getStyleClass().add("add-button");
+        submitButton.setOnAction(event -> {
+            addNewTodo(todos, todoListView, addTodoInput);
+        });
+
+        addTodoBox.getChildren().addAll(addTodoInput, newTodoSpacer, submitButton);
+
+
+
+        // ajoute les element de l'app dans le conteneur principale:
+        mainVBox.getChildren().addAll(title, tabs, addTodoBox, todoListView);
 
         // Scene:
         Scene scene = new Scene(mainVBox);
